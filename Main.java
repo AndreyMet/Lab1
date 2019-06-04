caafcbb1160808c8787c2e38b2a59498aae36ce7
AndreyMet <AM211097@yandex.ru>
2019-06-04 20:01:40 +0300

    An

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-		Person p1 = new Person("Voronova", "Elena");
+		Person p1 = new Person("Voro", "Elena");
         Person p2 = new Person("Belova", "Valeria");
-        Person p3 = new Person("Ivanov", "Mark");
+        Person p3 = new Person("Ind", "Mark");
         Company c1 = new Company("Roga&Kopyta");
         c1.setBoss(p3);
         c1.addEmployee(p1);
